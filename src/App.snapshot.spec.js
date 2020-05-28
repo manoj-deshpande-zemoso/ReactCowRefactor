d733df6b105e7519d40f77293ddf7999b18014f0
FBerthelot <florent.berthelot35@gmail.com>
2020-05-28 12:22:05 +0200

    Step 1

--- src/App.snapshot.spec.js
+++ src/App.snapshot.spec.js
@@ -1,10 +1,42 @@
 import App from "./App";
 import React from "react";
 import renderer from "react-test-renderer";
+import {shallow} from "component-test-utils-react";
+import {render} from "@testing-library/react";
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-describe("App - Reafactoring Tests", () => {
+Enzyme.configure({ adapter: new Adapter() });
+
+// React test render
+describe("App - react-test-render - Reafactoring Tests", () => {
   it("should look the same", () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+});
+
+
+// Enzyme
+describe("App - enzyme - Reafactoring Tests", () => {
+  it("should look the same", () => {
+    const component = mount(<App />);
+    expect(component.html()).toMatchSnapshot();
+  });
+});
+
+// component-test-utils-react
+describe("App - component-test-utils-react - Reafactoring Tests", () => {
+  it("should look the same", () => {
+    const component = shallow(<App />, {blackList: true});
+    expect(component.html()).toMatchSnapshot();
+  });
+});
+
+// @testing-library
+describe("App - @testing-library/react - Reafactoring Tests", () => {
+  it("should look the same", () => {
+    const {container} = render(<App />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
